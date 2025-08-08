67402929c1a41177036787c1341bb4df7bfc4931
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2025-08-08 17:58:53 -0500

    Built the defaults and the header.

--- Web/app.js
+++ Web/app.js
@@ -6,7 +6,6 @@ var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,11 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Web/sitemap.md
@@ -0,0 +1,8 @@
+Home/landing
+Pricing
+Docs/how to use
+
+Sign in to existing account/sign up
+Account dashboard (See/manage the software package purchased, purchase if one has not been bought yet, button to access app)
+
+Purchase the software
